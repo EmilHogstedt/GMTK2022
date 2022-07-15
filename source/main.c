4b29a2b9c6640be20b6a941cb72bd9517dec5a54
EmilHogstedt <emil.hogstedt@hotmail.com>
2022-07-15 19:35:48 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -2,6 +2,10 @@
 
 #define MAX_COLUMNS 20
 
+void Run(void);
+void Update(void);
+void Draw(void);
+
 int main(void)
 {
     const int screenWidth = 1600;
@@ -31,39 +35,55 @@ int main(void)
 
     SetCameraMode(camera, CAMERA_FIRST_PERSON);
 
-    while (!WindowShouldClose())
-    {
-        UpdateCamera(&camera);
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera);
-
-        DrawPlane((Vector3) { 0.0f, 0.0f, 0.0f }, (Vector2) { 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
-        DrawCube((Vector3) { -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
-        DrawCube((Vector3) { 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
-        DrawCube((Vector3) { 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
-
-        // Draw some cubes around
-        for (int i = 0; i < MAX_COLUMNS; i++)
-        {
-            DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-            DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-        }
-
-        EndMode3D();
-
-        DrawRectangle(10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(10, 10, 220, 70, BLUE);
-
-        DrawText("First person camera default controls:", 20, 20, 10, BLACK);
-        DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
-        DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
-
-        EndDrawing();
-    }
+    Run();
+    
     CloseWindow();
 
     return 0;
+}
+
+void Run(void)
+{
+    while (!WindowShouldClose())
+    {
+        Update();
+        Draw();
+    }
+}
+
+void Update(void)
+{
+    UpdateCamera(&camera);
+}
+
+void Draw(void)
+{
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    BeginMode3D(camera);
+
+    DrawPlane((Vector3) { 0.0f, 0.0f, 0.0f }, (Vector2) { 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
+    DrawCube((Vector3) { -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
+    DrawCube((Vector3) { 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
+    DrawCube((Vector3) { 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
+
+    // Draw some cubes around
+    for (int i = 0; i < MAX_COLUMNS; i++)
+    {
+        DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+        DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+    }
+
+    EndMode3D();
+
+    DrawRectangle(10, 10, 220, 70, Fade(SKYBLUE, 0.5f));
+    DrawRectangleLines(10, 10, 220, 70, BLUE);
+
+    DrawText("First person camera default controls:", 20, 20, 10, BLACK);
+    DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
+    DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
+
+    EndDrawing();
 }
